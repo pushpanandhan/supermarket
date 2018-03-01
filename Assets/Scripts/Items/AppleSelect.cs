df0d4cb0a9883ec790c96b64efb750da2f20b822
Eathindhar Mahendradevan <cldveathi@gmail.com>
2018-03-02 00:41:03 +0530

    first commit

---
+++ Assets/Scripts/Items/AppleSelect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleSelect : MonoBehaviour {
+	public static int appleCount = 0;
+
+	void OnMouseDown(){
+		appleCount++;
+		Debug.Log (appleCount);
+	}
+}
